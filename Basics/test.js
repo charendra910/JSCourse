f0f6251f35077ebe62f9274b11060c68058ee211
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-01 18:04:44 +0000

    update

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,11 @@
-console.log("Harendra");
+const har=34324234;
+//har =123213;
+let ha="har"
+ha=123213;
+var ma="afhdf"
+ma=13434790
+// console.log(har);
+// console.log(ha)
+// console.log(ma)
+
+console.table([har,ha,ma])

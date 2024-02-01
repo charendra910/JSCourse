7323c2d8f2abc574b21bfdd7eaf43fca11b9cf78
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-01 19:43:10 +0000

    update

---
+++ Basics/array1.js
@@ -0,0 +1,25 @@
+let myArr = [1,2,3,4,5,6]
+
+//array methods
+
+// console.log(myArr)
+// console.log(myArr.push(7));
+// console.log(myArr.pop());
+// console.log(myArr[2])
+// console.log(myArr.length)
+
+// myArr.unshift(0)  //add 0 at index 0
+// console.log(myArr)
+
+// myArr.shift(1)
+// console.log(myArr);
+
+// let arr1=myArr.slice(1,3)
+// console.log("A",arr2);
+
+// let arr2 = myArr.splice(4,5)
+// console.log("B",arr2);
+
+//const myn1=myArr.splice(1,3)
+//console.log("B",myArr);
+console.log(myn1);

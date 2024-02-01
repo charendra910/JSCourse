1e446542a3709118e1e377a36241fa73ab990197
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-01 18:04:27 +0000

    update math

---
+++ Basics/math.js
@@ -0,0 +1,21 @@
+//console.log(Math.abs(-4)); //convert to 4
+//console.log(Math.round(4.7));
+
+// console.log(Math.ceil(4.7));
+// console.log(Math.ceil(4.1));
+
+// console.log(Math.round(4.7));
+// console.log(Math.round(4.5));
+
+// console.log(Math.min(4,2,3,7));
+// console.log(Math.max(4,1,3,6,7,8,9,10,11));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10+1); 
+//console.log(Math.floor(Math.random()*10+1));
+
+const min=10;
+const max=20;
+let bet = Math.random()* ((max-min+1))
+console.log(Math.floor(bet) + min);
---
+++ Basics/str1.js
@@ -0,0 +1,21 @@
+let st= new String("Harendra")
+// console.log(st.indexOf('a'));
+ //console.log(st.charAt(3));
+
+ let st2=st.slice(1,4)   //can use to print reverse but not in the substring
+let st3=st.substring(8,6)
+ //console.log(st2);
+
+ let asd="   agsdasgd   "
+ //console.log(asd.trim()) //remove the space by trim
+
+//console.log(asd.replace("d","b"));
+//console.log(asd.includes("ags")); // search weather ags is in the string or not if yes then true and no for the false if not available
+
+let a =301.923;
+//console.log(a.toFixed(1)); //302
+//console.log(a.toPrecision(2));
+
+let b=1000000000;
+console.log(b.toLocaleString());
+console.log(b.toString());

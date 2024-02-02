52aa3127004588b457652f658914c83e3eaad7f6
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-02 17:49:22 +0000

    changes in array2

--- Basics/array2.js
+++ Basics/array2.js
@@ -19,8 +19,33 @@ const dc =["q","s","t","u","v"];
 // console.log(Array.isArray("a","b","c","d","e","f"));   //false mean it is not array
 // console.log(Array.from("Harendra")); //convert harendra to array
 
-let a=12312;
-let b="Harendra";
-let c=[1,2,"hdsf"];
-let d=Array.of(a,b,c);
-console.log(d);
+// let a=12312;
+// let b="Harendra";
+// let c=[1,2,"hdsf"];
+// let d=Array.of(a,b,c);
+// console.log(d);
+
+let mySym = Symbol("date"); // symbol as data type
+
+let myUser={
+    id:1,
+    [mySym]: "dhjjgfdf",        // we assess the above symbol by writing in []
+    name:"Harendra",
+    "email":"harendra@gmail.com",
+    roll:10,
+    class : [
+        "hari","monday"
+    ],
+};
+
+myUser.name = "RadheKrishna"
+Object.freeze(myUser);   // once its freeze then value can't be override
+console.log(myUser);
+
+myUser.name = "Radhe"
+console.log(myUser);
+
+// console.log(myUser[mySym]);   //acess with in bracket
+// console.log(myUser.name);
+// console.log(myUser.email);
+// console.log(myUser["name"]);   //bcz name is readed as string 

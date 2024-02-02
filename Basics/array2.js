2eedef63ddf61e069ac84bcbb81db080e816b666
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-02 16:52:38 +0000

    update

---
+++ Basics/array2.js
@@ -0,0 +1,26 @@
+const marvel = ["a","b","c","d","e","f"];
+const dc =["q","s","t","u","v"];
+
+// marvel.push(dc);
+// console.log(marvel);
+
+// const bth=marvel.concat(dc);   
+// console.log(bth);      
+
+// let spread = [...marvel, ...dc];
+// console.log(spread);
+
+// let ar1= ["a","b",1,34,45,6,[1,23,4],[234,"sdf",56,[11,22,33,"45"],{a:"as",name:"harendra"},true,"moon"],1,3]
+// let combine = ar1.flat(Infinity);
+// console.log(combine);
+// console.log(ar1.length); 
+
+
+// console.log(Array.isArray("a","b","c","d","e","f"));   //false mean it is not array
+// console.log(Array.from("Harendra")); //convert harendra to array
+
+let a=12312;
+let b="Harendra";
+let c=[1,2,"hdsf"];
+let d=Array.of(a,b,c);
+console.log(d);

20454edbff9b4445dcf3da9eef8f6007d6af5575
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-02 18:01:53 +0000

    update objects

--- Basics/array2.js
+++ Basics/array2.js
@@ -24,28 +24,3 @@ const dc =["q","s","t","u","v"];
 // let c=[1,2,"hdsf"];
 // let d=Array.of(a,b,c);
 // console.log(d);
-
-let mySym = Symbol("date"); // symbol as data type
-
-let myUser={
-    id:1,
-    [mySym]: "dhjjgfdf",        // we assess the above symbol by writing in []
-    name:"Harendra",
-    "email":"harendra@gmail.com",
-    roll:10,
-    class : [
-        "hari","monday"
-    ],
-};
-
-myUser.name = "RadheKrishna"
-Object.freeze(myUser);   // once its freeze then value can't be override
-console.log(myUser);
-
-myUser.name = "Radhe"
-console.log(myUser);
-
-// console.log(myUser[mySym]);   //acess with in bracket
-// console.log(myUser.name);
-// console.log(myUser.email);
-// console.log(myUser["name"]);   //bcz name is readed as string 
---
+++ Basics/obj2.js
@@ -0,0 +1,9 @@
+const myUser = new Object()
+
+const user = {}
+
+console.log(myUser);
+console.log(typeof myUser);
+
+console.log(user);
+console.log(typeof user);

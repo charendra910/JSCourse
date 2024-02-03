bdce1faac7aa0bf657e01767e5670870e538c3fb
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-03 16:37:08 +0000

    update iifee commit

---
+++ Basics/iife.js
@@ -0,0 +1,10 @@
+//immediately invoked function expression (IIFE)      //iife is writen to show the function after windows
+
+(function name(){
+    console.log("Hello user")                    //named iife
+}) ();
+
+
+((email) =>{
+    console.log(`Your email id is ${email}!`)       //without named iife
+}) ("Harendra")

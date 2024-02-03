61aeae22986c8712a8c39af800ebf7fbd2016028
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-03 12:37:29 +0000

    update

--- Basics/obj2.js
+++ Basics/obj2.js
@@ -1,9 +1,102 @@
-const myUser = new Object()
+// const myUser = new Object()
 
-const user = {}
+// const user = {}
 
-console.log(myUser);
-console.log(typeof myUser);
+// console.log(myUser);
+// console.log(typeof myUser);
 
-console.log(user);
-console.log(typeof user);
+// console.log(user);
+// console.log(typeof user);
+
+
+
+// const tinderUser = new Object()
+// const tinderUser = {}
+// tinderUser.id = "123abc"
+// tinderUser.name = "Sammy"
+// tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+
+
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",      //json format fetching api
+//     "price": "free"
+// }
+
+[
+    {},
+    {},                 //json format
+    {}
+]

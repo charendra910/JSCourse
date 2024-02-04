0dec84bb2e807b910024b8051fb9d10519807fab
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-02-04 17:40:46 +0000

    commit change

--- Basics/obj_in_array.js
+++ Basics/obj_in_array.js
@@ -33,3 +33,17 @@ let newNumber = numbers.map ( (add) => {
 })
 
 console.log(newNumber);
+
+
+
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
